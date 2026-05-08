1e3d85a0ceb0d528983284bc017463e69ffbf557
tudor barbu <tudorbarbu123@yahoo.com>
2026-05-08 19:10:06 +0300

    https://proiect-mds.atlassian.net/browse/DEV-16?atlOrigin=eyJpIjoiMjQxN2IyYWZhZTA2NDA3YzkwMWNjZjNkZTMzN2RlMWEiLCJwIjoiaiJ9

--- vacation-planner/frontend/src/app/page.tsx
+++ vacation-planner/frontend/src/app/page.tsx
@@ -1,5 +1,31 @@
-import { redirect } from "next/navigation";
+"use client";
+
+import { useRouter } from "next/navigation";
+import { MarketingHero } from "@/components/marketing/MarketingHero";
+import { SocialProof } from "@/components/marketing/SocialProof";
+import { FeatureGrid } from "@/components/marketing/FeatureGrid";
+import { ProductPreview } from "@/components/marketing/ProductPreview";
+import { DestinationGrid } from "@/components/marketing/DestinationGrid";
+import { HowItWorks } from "@/components/marketing/HowItWorks";
+import { CollaborationSection } from "@/components/marketing/CollaborationSection";
+import { MarketingCTA } from "@/components/marketing/MarketingCTA";
+import { MarketingFooter } from "@/components/marketing/MarketingFooter";
 
 export default function Home() {
-  redirect("/plan");
+  const router = useRouter();
+  const goToPlanner = () => router.push("/plan");
+
+  return (
+    <div className="flex min-h-screen flex-col bg-[#020204] text-white">
+      <MarketingHero onStartPlanning={goToPlanner} />
+      <SocialProof />
+      <FeatureGrid />
+      <ProductPreview />
+      <DestinationGrid />
+      <HowItWorks />
+      <CollaborationSection />
+      <MarketingCTA onStartPlanning={goToPlanner} />
+      <MarketingFooter />
+    </div>
+  );
 }
